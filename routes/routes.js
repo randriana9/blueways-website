2de9ac4153646dcc40b3c2c517f41994cc962f5c
randriana <rina.andriana@outlook.com>
2017-02-01 09:44:44 +0100

    converted all view to pug

--- routes/routes.js
+++ routes/routes.js
@@ -18,8 +18,20 @@ router.get('/contact', function(req, res, next) {
   res.render('contact.html');
 });*/
 
-router.get('/', function(req, res) {
-  res.render('index')
+router.get('/', function(req, res, next) {
+  res.render('index');
+})
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+})
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects');
+})
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
 })
 
 module.exports = router;
