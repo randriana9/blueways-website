63b1a88f4ecb12b387e550b64a15acd2995c012c
randriana <rina.andriana@outlook.com>
2017-02-24 19:18:28 +0100

    changed url and added cdn jquery

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,92 @@ var gulp = require('gulp'),
 var bs = require('browser-sync').create();
 
 
-    gulp.task('pug-compile', function() {
-        return gulp.src('views/**/*.pug')
-        .pipe(pug({}))
-         .pipe(gulp.dest('dist/views'));
+
+    gulp.task('build', ['pug-compile', 'styles-to-dist', 'copy-vendors-to-dist', 'copy-images']);
+
+    gulp.task('pre-build', function() {
+        /* top files */
+         gulp.src('views/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('pre-build'));
+
+        /* includes files */
+        gulp.src('views/includes/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('pre-build'));
+
+        /* english files */
+        gulp.src('views/lang-en/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('pre-build'));
+
+        /* french files */
+        gulp.src('views/lang-fr/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('pre-build'));
+
+        /* layouts files */
+        gulp.src('views/layouts/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('pre-build'));
     })
+
+    gulp.task('pug-compile', function() {
+        /* top files */
+         gulp.src('pre-build/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('dist'));
+    })
+
+    gulp.task('styles-to-dist', function() {
+        return gulp.src('./src/sass/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('dist/stylesheets'))
+    })
+
+    gulp.task('copy-images', function() {
+        return gulp.src('public/images/**/*.*')
+                .pipe(gulp.dest('dist/images'))
+    })
+
+    gulp.task('copy-vendors-to-dist', function() {
+        /* copy bootstrap javascripts */
+        gulp.src(['node_modules/bootstrap-sass/assets/javascripts/**'])
+        .pipe(gulp.dest('dist/javascripts/bootstrap'));
+
+         /* copy bootstrap stylesheets */
+        gulp.src(['node_modules/bootstrap-sass/assets/stylesheets/**'])
+        .pipe(gulp.dest('dist/stylesheets/bootstrap'));
+
+        /* copy fullpage.js stylesheets */
+        gulp.src(['node_modules/fullpage.js/dist/jquery.fullpage.min.css', 'node_modules/fullpage.js/dist/jquery.fullpage.css'])
+        .pipe(gulp.dest('dist/stylesheets/fullpage.js'));
+
+        /* copy fullpage.js javascripts */
+        gulp.src(['node_modules/fullpage.js/dist/jquery.fullpage.min.js', 'node_modules/fullpage.js/dist/jquery.fullpage.js'])
+        .pipe(gulp.dest('dist/javascripts/fullpage.js'));
+
+        /* copy scrollreveal */
+        gulp.src(['node_modules/scrollreveal/dist/scrollreveal.min.js'])
+        .pipe(gulp.dest('dist/javascripts/scrollreveal'));
+
+        gulp.src('public/javascripts/jquery-custom-plugins.js')
+        .pipe(gulp.dest('dist/javascripts'));
+
+    })
+
 
     gulp.task('sass', function() {
         return gulp.src('./src/sass/*.scss')
@@ -36,9 +117,10 @@ var bs = require('browser-sync').create();
 
     gulp.task('copy', function() {
 
-        /* copy query */
+        /* copy query 
         gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('public/javascripts/jquery'));
+        */
 
         /* copy bootstrap javascripts */
         gulp.src(['node_modules/bootstrap-sass/assets/javascripts/**'])
